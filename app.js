dfc279d0a75b9373eccafc273a041cf0300daf56
Franko Stipanov <fstipanov@gmail.com>
2021-05-20 09:29:16 +0200

    rgb - work

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const r = document.querySelector("#r");
-const g = document.querySelector("#g");
-const b = document.querySelector("#b");
-console.log(r.value);
+const r = document.querySelector('#r');
+const g = document.querySelector('#g');
+const b = document.querySelector('#b');
+const colorBox = document.querySelector('.color__box');
+const rgb = document.querySelector('.rgb');
 
-r.addEventListener("change", () => {
-  console.log(r.value);
-  console.log(g.value);
-  console.log(b.value);
-});
+const color = () => {
+  const rgb = `${r.value},${g.value},${b.value}`;
+  return rgb;
+};
+
+const changeColor = () => {
+  let boxColor = color();
+  colorBox.style.backgroundColor = `rgb(${boxColor})`;
+  rgb.innerHTML = `rgb(${boxColor})`;
+};
+
+r.addEventListener('change', changeColor);
+g.addEventListener('change', changeColor);
+b.addEventListener('change', changeColor);
