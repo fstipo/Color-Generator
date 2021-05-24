7a26c22841d25ae7260b4f93c9bf27e746c47995
Franko Stipanov <fstipanov@gmail.com>
2021-05-24 08:50:27 +0200

    hex working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const g = document.querySelector('#g');
 const b = document.querySelector('#b');
 const colorBox = document.querySelector('.color__box');
 const rgb = document.querySelector('.rgb');
+const hex = document.querySelector('.hex');
 
 const color = () => {
   const rgb = `${r.value},${g.value},${b.value}`;
@@ -13,22 +14,15 @@ const changeColor = () => {
   let boxColor = color();
   colorBox.style.backgroundColor = `rgb(${boxColor})`;
   rgb.innerHTML = `rgb(${boxColor})`;
+  let hexR = Number(r.value);
+  let hexG = Number(g.value);
+  let hexB = Number(b.value);
+  const hexColor = `#${hexR.toString(16)}${hexG.toString(16)}${hexB.toString(
+    16
+  )}`;
+  hex.innerHTML = hexColor;
 };
 
 r.addEventListener('change', changeColor);
 g.addEventListener('change', changeColor);
 b.addEventListener('change', changeColor);
-
-const changeHex = () => {
-  let rgb = color();
-  console.log(rgb);
-  rgbArray = rgb.split(',');
-  console.log(rgbArray);
-  for (let i = 0; i < rgbArray.length; i++) {
-    let hex = Number(rgbArray[i].toString(16));
-    console.log(hex);
-    console.log(rgbArray[i]);
-  }
-};
-
-changeHex();
